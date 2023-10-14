ae744f7e81e7d78624dbe70ecad3af554cf327ac
Dawid <dawid1234x@wp.pl>
2023-10-14 21:56:50 +0200

    feat(frontend): Event and home view

--- web/src/app/home/home.component.ts
+++ web/src/app/home/home.component.ts
@@ -8,9 +8,43 @@ import {ShortEventModel} from "./shortEvent.model";
 })
 export class HomeComponent {
   shortEvents: ShortEventModel[] = [
-    new ShortEventModel('tytul','opis','https://upload.wikimedia.org/wikipedia/commons/7/7a/Rynek_Ko%C5%9Bciuszki%2C_Bia%C5%82ystok_%282%29.jpg',new Date()),
-    new ShortEventModel('tytul','opis','https://upload.wikimedia.org/wikipedia/commons/7/7a/Rynek_Ko%C5%9Bciuszki%2C_Bia%C5%82ystok_%282%29.jpg',new Date()),
-    new ShortEventModel('tytul','opis','https://upload.wikimedia.org/wikipedia/commons/7/7a/Rynek_Ko%C5%9Bciuszki%2C_Bia%C5%82ystok_%282%29.jpg',new Date())
+    new ShortEventModel('tytul','opisopisopis opis opisopisopis opis','https://upload.wikimedia.org/wikipedia/commons/7/7a/Rynek_Ko%C5%9Bciuszki%2C_Bia%C5%82ystok_%282%29.jpg',new Date()),
+    new ShortEventModel('tytul','opisopisopis opisopisopisopisopisopisopisopisopis opisopisopisopisopisopisopisopis','https://upload.wikimedia.org/wikipedia/commons/7/7a/Rynek_Ko%C5%9Bciuszki%2C_Bia%C5%82ystok_%282%29.jpg',new Date()),
+    new ShortEventModel('tytul','opisopisopisopis opisopisopisopis opisopisopisopisopis opisopisopis','https://upload.wikimedia.org/wikipedia/commons/7/7a/Rynek_Ko%C5%9Bciuszki%2C_Bia%C5%82ystok_%282%29.jpg',new Date())
   ];
+
+  currentIndex: number = 0;
+  isWideScreen: boolean = window.innerWidth >= 800;
+
+  get visibleEvents(): ShortEventModel[] {
+    const wrappedIndex = this.currentIndex % this.shortEvents.length;
+
+    if (this.isWideScreen) {
+      const newEvent = this.shortEvents[wrappedIndex];
+      const prevEvents = [
+        this.shortEvents[(wrappedIndex - 1 + this.shortEvents.length) % this.shortEvents.length],
+        this.shortEvents[(wrappedIndex - 2 + this.shortEvents.length) % this.shortEvents.length],
+      ];
+      return [newEvent, ...prevEvents];
+    } else {
+      return this.shortEvents.slice(wrappedIndex, wrappedIndex + 1);
+    }
+  }
+
+  ngOnInit() {
+    window.addEventListener('resize', this.onResize.bind(this));
+  }
+
+  onResize() {
+    this.isWideScreen = window.innerWidth >= 800;
+  }
+
+  prevEvent() {
+    this.currentIndex = (this.currentIndex - 1 + this.shortEvents.length) % this.shortEvents.length;
+  }
+
+  nextEvent() {
+    this.currentIndex = (this.currentIndex + 1) % this.shortEvents.length;
+  }
 
 }
