6bd8085a68b482d2903b8c727840f5c106903140
Modzel200 <49248797+Modzel200@users.noreply.github.com>
2023-10-18 16:16:32 +0200

    homeComponent

--- web/src/app/user-panel/user-panel.component.ts
+++ web/src/app/user-panel/user-panel.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserPanelService} from "./service/user-panel.service";
 import {PostToAdd, Tags} from "./model/user-panel.model";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-panel',
   templateUrl: './user-panel.component.html',
   styleUrls: ['./user-panel.component.scss']
 })
-export class UserPanelComponent {
+export class UserPanelComponent implements OnInit{
   tags: Tags[] =[{
     name: 'pub'
   }
@@ -21,12 +22,24 @@ export class UserPanelComponent {
     tags: this.tags,
     link: ''
   }
-  constructor(private userPanelService: UserPanelService) {
+  constructor(private userPanelService: UserPanelService, private router: Router) {
   }
+  ngOnInit() {
+    if(localStorage.getItem("Authorization")==null)
+    {
+      this.router.navigate(['/']);
+    }
+  }
+
   onSubmit()
   {
     this.userPanelService.addNewPost(this.postToAdd).subscribe(response=>{
       console.log(response);
     });
+  }
+  logOff()
+  {
+    localStorage.clear();
+    window.location.reload();
   }
 }
