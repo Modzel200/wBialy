aa95161927ceb23f7b119bee41d9a15b7c0dfd9c
Dawid <dawid1234x@wp.pl>
2023-12-17 22:08:21 +0100

    feat(frontend): Mobile event reponsivity

--- web/src/app/events/events.component.ts
+++ web/src/app/events/events.component.ts
@@ -1,12 +1,12 @@
-import {Component, OnInit, } from '@angular/core';
-import {EventPost} from "./model/event.model";
-import {EventsService} from "./service/events.service";
-import {PageResultModel} from "./model/pageResult.model";
-import {map} from 'rxjs/operators'
-import {EventComponent} from "./event/event.component";
-import {DatePipe, ViewportScroller} from "@angular/common";
-import {Router} from "@angular/router";
-import {MatDialog} from "@angular/material/dialog";
+import { Component, OnInit, } from '@angular/core';
+import { EventPost } from "./model/event.model";
+import { EventsService } from "./service/events.service";
+import { PageResultModel } from "./model/pageResult.model";
+import { map } from 'rxjs/operators'
+import { EventComponent } from "./event/event.component";
+import { DatePipe, ViewportScroller } from "@angular/common";
+import { Router } from "@angular/router";
+import { MatDialog } from "@angular/material/dialog";
 import { FormControl } from '@angular/forms';
 import { UserPanelService } from '../user-panel/service/user-panel.service';
 import { Tags } from '../user-panel/model/user-panel.model';
@@ -17,25 +17,25 @@ import { faFilter } from '@fortawesome/free-solid-svg-icons'
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
-export class EventsComponent implements OnInit{
+export class EventsComponent implements OnInit {
   number = 1;
   allTags: Tags[] = []
   events: EventPost[] = [];
-  pageResult: PageResultModel={
+  pageResult: PageResultModel = {
     items: [],
-    totalPages:0,
-    itemFrom:0,
-    itemTo:0,
-    totalItemsCount:0
+    totalPages: 0,
+    itemFrom: 0,
+    itemTo: 0,
+    totalItemsCount: 0
   };
   toppings = new FormControl();
   selectedToppings = [];
   sorting = new FormControl();
   selectedSort = '';
-  selectedToppingsString:string[] = [];
+  selectedToppingsString: string[] = [];
   faFilter = faFilter;
 
-  constructor(private eventsService: EventsService, private datePipe: DatePipe, private router: Router, public dialog: MatDialog,private scroller: ViewportScroller,private userPanelService: UserPanelService,) {
+  constructor(private eventsService: EventsService, private datePipe: DatePipe, private router: Router, public dialog: MatDialog, private scroller: ViewportScroller, private userPanelService: UserPanelService,) {
   }
   ngOnInit() {
     this.getAllPosts();
@@ -43,74 +43,69 @@ export class EventsComponent implements OnInit{
   }
   canGoNext: boolean = false;
   canGoPrev: boolean = false;
-  classmode = localStorage.getItem('DarkMode') === 'true'? 'dark-mode' : '';
+  classmode = localStorage.getItem('DarkMode') === 'true' ? 'dark-mode' : '';
   selected: Date | null | string = null;
 
-  formatDate(){
+  formatDate() {
     this.selected = this.datePipe.transform(this.selected, 'YYYY-MM-dd');
   }
-  getAllTags()
-  {
+  getAllTags() {
     this.eventsService.getAllTags()
-      .subscribe(response=>{
+      .subscribe(response => {
         this.allTags = response;
       })
   }
 
-  getAllPosts(){
+  getAllPosts() {
     this.canGoNextPage();
     this.canGoPrevPage();
-    this.eventsService.getAllPosts(this.selectedToppingsString,this.selected,this.number,this.selectedSort)
+    this.eventsService.getAllPosts(this.selectedToppingsString, this.selected, this.number, this.selectedSort)
       .subscribe(response => {
-      this.pageResult = response;
-      this.events = this.pageResult.items;
-      this.changeDateFormat();
-    });
+        this.pageResult = response;
+        this.events = this.pageResult.items;
+        this.changeDateFormat();
+      });
   }
 
-  sendFilters(){
+  sendFilters() {
     this.selectedToppingsString = this.selectedToppings;
     this.getAllPosts();
   }
-  clearTags()
-  {
+  clearTags() {
     this.selectedToppings = [];
     this.selected = "";
     this.getAllPosts();
   }
   canGoNextPage(): void {
-    this.eventsService.getAllPosts(this.selectedToppingsString,this.selected,(this.number) + 1, this.selectedSort)
+    this.eventsService.getAllPosts(this.selectedToppingsString, this.selected, (this.number) + 1, this.selectedSort)
       .subscribe(response => {
         this.canGoNext = response.items.length === 0 ? true : false;
       });
   }
 
   canGoPrevPage(): void {
-    this.canGoPrev = this.number === 1? true : false;
+    this.canGoPrev = this.number === 1 ? true : false;
   }
 
-  nextPage(){
+  nextPage() {
     this.number++;
     this.getAllPosts();
     this.scroller.scrollToAnchor("eventsScroll");
   }
-  backPage(){
+  backPage() {
     this.number--;
     this.getAllPosts();
     this.scroller.scrollToAnchor("eventsScroll");
   }
-  changeDateFormat()
-  {
-    for(let i=0;i<this.events.length;i++)
-    {
+  changeDateFormat() {
+    for (let i = 0; i < this.events.length; i++) {
       this.events[i].eventDate = <string>this.datePipe.transform(this.events[i].eventDate, 'dd.MM.yyyy hh:mm');
     }
   }
-  showEvent(event: EventPost){
-    const classmode = localStorage.getItem('DarkMode') === 'true'? 'dark-mode' : '';
+  showEvent(event: EventPost) {
+    const classmode = localStorage.getItem('DarkMode') === 'true' ? 'dark-mode' : '';
     this.eventsService.event = event;
-    const dialogRef = this.dialog.open(EventComponent,{
-      height:'80%',
+    const dialogRef = this.dialog.open(EventComponent, {
       autoFocus: false,
       panelClass: classmode
     });
