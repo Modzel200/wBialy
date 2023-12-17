257f37fd536fd0c807d965ab98c12281daf0fe65
Modzel200 <49248797+Modzel200@users.noreply.github.com>
2023-12-17 23:24:54 +0100

    android app

--- web/capacitor.config.ts
+++ web/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'wbialyApka',
+  appName: 'wBialy',
   webDir: 'dist/web',
   server: {
     androidScheme: 'https'
